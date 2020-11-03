45847c4bf6d4c2574526f8e9dbbace17216980de
adjidedjo <adji.el.djo@gmail.com>
2020-11-03 08:46:33 +0700

    fix

--- app/controllers/concerns/paramdates_concern.rb
+++ app/controllers/concerns/paramdates_concern.rb
@@ -4,27 +4,27 @@ module ParamdatesConcern
   
   class_methods do
     def two_months_month
-      2.months.ago.beginning_of_month.to_date.month
+      (2.months.ago.to_date - 6).beginning_of_month.to_date.month
     end
   
     def two_months_date
-      2.months.ago.beginning_of_month.to_date
+      (2.months.ago.to_date - 6).beginning_of_month.to_date
     end
   
     def two_months_year
-      2.months.ago.beginning_of_month.to_date.year
+      (2.months.ago.to_date - 6).beginning_of_month.to_date.year
     end
   
     def last_month_date
-      1.month.ago.beginning_of_month.to_date
+      (1.months.ago.to_date - 6).beginning_of_month.to_date
     end
   
     def last_month_month
-      1.month.ago.beginning_of_month.to_date.month
+      (1.months.ago.to_date - 6).beginning_of_month.to_date.month
     end
   
     def last_month_year
-      1.month.ago.beginning_of_month.to_date.year
+      (1.months.ago.to_date - 6).beginning_of_month.to_date.year
     end
   
     def this_month_date
