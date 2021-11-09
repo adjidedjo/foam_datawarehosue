aa1d614c94faa9d4c1a4c834a22c375ce6bdd23a
adjidedjo <adji.el.djo@gmail.com>
2021-11-09 13:17:39 +0700

    fix

--- app/models/nasional_sales.rb
+++ app/models/nasional_sales.rb
@@ -19,12 +19,14 @@ class NasionalSales < ActiveRecord::Base
   end
 
   def self.foam_nasional_channel_total(date)
-    self.find_by_sql("SELECT lc.val_1, lc.val_2, lc.channel,
+    self.find_by_sql("SELECT lc.val_1, lc.val_2, lc.channel, lc.kubikasi1, lc.kubikasi2,
       ROUND((((lc.val_1 - lc.val_2) / lc.val_2) * 100), 0) AS percentage FROM
       (
         SELECT area_desc AS areas, channel AS channel,
           SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN total_sales END) val_2,
-          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) val_1
+          SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN kubikasi END) kubikasi2,
+          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) val_1,
+          SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN kubikasi END) kubikasi1
           FROM foam_bychannel WHERE tanggalsj BETWEEN '#{two_months_date(date)}' AND '#{this_month_date(date)}'
           GROUP BY channel
       ) AS lc
@@ -32,12 +34,14 @@ class NasionalSales < ActiveRecord::Base
   end
 
   def self.foam_nasional_this_month_total(date)
-    self.find_by_sql("SELECT lc.val_1, lc.val_2, lc.brand,
+    self.find_by_sql("SELECT lc.val_1, lc.val_2, lc.brand, lc.kubikasi1, lc.kubikasi2,
       ROUND((((lc.val_1 - lc.val_2) / lc.val_2) * 100), 0) AS percentage FROM
       (
         SELECT area_desc AS areas, brand AS brand,
           SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN total_sales END) val_2,
-          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) val_1
+          SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN kubikasi END) kubikasi2,
+          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) val_1,
+          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN kubikasi END) kubikasi1
           FROM foam_bybrands WHERE tanggalsj BETWEEN '#{two_months_date(date)}' AND '#{this_month_date(date)}' GROUP BY brand
       ) AS lc
     ")
@@ -46,8 +50,11 @@ class NasionalSales < ActiveRecord::Base
   def self.recap_bysubbrand(date)
     find_by_sql("SELECT subbrand,
       SUM(CASE WHEN dmonth = '#{two_months_month(date)}' AND dyear = '#{two_months_year(date)}' THEN total_sales END) month2,
+      SUM(CASE WHEN dmonth = '#{two_months_month(date)}' AND dyear = '#{two_months_year(date)}' THEN kubikasi END) kubikasi2,
       SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN total_sales END) month1,
-      SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) monthnow
+      SUM(CASE WHEN dmonth = '#{two_months_month(date)}' AND dyear = '#{two_months_year(date)}' THEN kubikasi END) kubikasi1,
+      SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) monthnow,
+      SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN kubikasi END) kubikasinow
       FROM foam_datawarehouse.foam_bysubbrands WHERE tanggalsj BETWEEN '#{two_months_date(date)}' AND '#{this_month_date(date)}'
       GROUP BY subbrand
     ")
