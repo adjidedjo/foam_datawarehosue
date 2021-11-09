9613363570693a44bb51cd69624159d70c3bc5d9
adjidedjo <adji.el.djo@gmail.com>
2021-11-09 13:26:45 +0700

    fix

--- app/models/channel_sales.rb
+++ app/models/channel_sales.rb
@@ -2,13 +2,16 @@ class ChannelSales < ActiveRecord::Base
   self.table_name = "sales_warehouses"
   include ParamdatesConcern
   def self.total_revenue_channel_foam(date, channel)
-    self.find_by_sql("SELECT lc.monthnow, lc.month1,
+    self.find_by_sql("SELECT lc.monthnow, lc.month1, lc.kubikasinow, lc.kubikasi1, lc.kubikasi2,
       ROUND((((lc.monthnow - lc.month1) / lc.month1) * 100), 0) AS percentage FROM
       (
         SELECT 'total',
         SUM(CASE WHEN dmonth = '#{two_months_month(date)}' AND dyear = '#{two_months_year(date)}' THEN total_sales END) month2,
+        SUM(CASE WHEN dmonth = '#{two_months_month(date)}' AND dyear = '#{two_months_year(date)}' THEN kubikasi END) kubikasi2,
         SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN total_sales END) month1,
-        SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) monthnow
+        SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN kubikasi END) kubikasi1,
+        SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) monthnow,
+        SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN kubikasi END) kubikasinow
         FROM foam_datawarehouse.foam_bybrands WHERE tanggalsj BETWEEN '#{two_months_date(date)}'
         AND '#{this_month_date(date)}' AND channel = '#{channel}'
       ) AS lc
@@ -30,12 +33,14 @@ class ChannelSales < ActiveRecord::Base
   end
 
   def self.foam_channel_this_month_total(date, channel)
-    self.find_by_sql("SELECT lc.val_1, lc.val_2, lc.brand,
+    self.find_by_sql("SELECT lc.val_1, lc.val_2, lc.brand, lc.kubikasi1, lc.kubikasi2,
       ROUND((((lc.val_1 - lc.val_2) / lc.val_2) * 100), 0) AS percentage FROM
       (
         SELECT area_desc AS areas, brand AS brand,
           SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN total_sales END) val_2,
-          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) val_1
+          SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN kubikasi END) kubikasi2,
+          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) val_1,
+          SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN kubikasi END) kubikasi1
           FROM foam_bybrands WHERE tanggalsj BETWEEN '#{two_months_date(date)}' AND '#{this_month_date(date)}'
           AND channel = '#{channel}' GROUP BY brand
       ) AS lc
@@ -43,11 +48,13 @@ class ChannelSales < ActiveRecord::Base
   end
 
   def self.recap_bysubbrand_channel(date, channel)
-    find_by_sql("SELECT rb.subbrand, rb.month1, rb.monthnow,
+    find_by_sql("SELECT rb.subbrand, rb.month1, rb.monthnow, rb.kubikasi1, rb.kubikasinow,
       ROUND((((rb.monthnow - rb.month1) / rb.month1) * 100), 0) AS percentage FROM
       ( SELECT subbrand,
         SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN total_sales END) month1,
-        SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) monthnow
+        SUM(CASE WHEN dmonth = '#{last_month_month(date)}' AND dyear = '#{last_month_year(date)}' THEN kubikasi END) kubikasi1,
+        SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN total_sales END) monthnow,
+        SUM(CASE WHEN dmonth = '#{this_month_month(date)}' AND dyear = '#{this_month_year(date)}' THEN kubikasi END) kubikasinow
         FROM foam_datawarehouse.foam_bysubbrands WHERE tanggalsj BETWEEN '#{two_months_date(date)}' AND '#{this_month_date(date)}'
         AND channel = '#{channel}' GROUP BY subbrand
       ) AS rb
