aefd48e3f25e79e7263efbf8994e2d1099b47676
Aji Yulianto <adji.el.djo@gmail.com>
2024-10-01 08:27:52 +0700

    fix date

--- app/controllers/concerns/paramdates_concern.rb
+++ app/controllers/concerns/paramdates_concern.rb
@@ -4,39 +4,39 @@ module ParamdatesConcern
   
   class_methods do
     def two_months_month(date)
-      ((date.to_date - 5).last_month.last_month).beginning_of_month.to_date.month
+      (date.to_date.last_month.last_month).beginning_of_month.to_date.month
     end
   
     def two_months_date(date)
-      ((date.to_date - 5).last_month.last_month).beginning_of_month.to_date
+      (date.to_date.last_month.last_month).beginning_of_month.to_date
     end
   
     def two_months_year(date)
-      ((date.to_date - 5).last_month.last_month).beginning_of_month.to_date.year
+      (date.to_date.last_month.last_month).beginning_of_month.to_date.year
     end
   
     def last_month_date(date)
-      ((date.to_date - 5).last_month).beginning_of_month.to_date
+      (date.to_date.last_month).beginning_of_month.to_date
     end
   
     def last_month_month(date)
-      ((date.to_date - 5).last_month).beginning_of_month.to_date.month
+      (date.to_date.last_month).beginning_of_month.to_date.month
     end
   
     def last_month_year(date)
-      ((date.to_date - 5).last_month).beginning_of_month.to_date.year
+      (date.to_date.last_month).beginning_of_month.to_date.year
     end
   
     def this_month_date(date)
-      (date.to_date - 5).end_of_month.to_date
+      date.to_date.end_of_month.to_date
     end
   
     def this_month_month(date)
-      (date.to_date - 5).end_of_month.to_date.month
+      date.to_date.end_of_month.to_date.month
     end
   
     def this_month_year(date)
-      (date.to_date - 5).end_of_month.to_date.year
+      date.to_date.end_of_month.to_date.year
     end
   end
 ########## END CHANNEL
